f3e71c66a36324bd05924b549dfa0f709270fdc8
Ola Rinta-Koski <github@rinta-koski.net>
2020-03-16 16:44:27 +0200

    initial commit

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+
+define("W5_TITLE", "W5Wiki");
+define("W5_VERSION", "0.1");
+define("W5_VERSION_DATE", "2020-03-16");
+
+define("W5_HOME", "/w5wiki/");
+define("W5_SOFTWARE_URL", "https://github.com/rockola/w5wiki/");
+
+/* internal */
+define("W5_CONTENT", "content/");
+define("W5_TAGS", array(
+    "TITLE" => W5_TITLE,
+    "VERSION" => W5_VERSION,
+    "VERSION_DATE" => W5_VERSION_DATE,
+    "HOME" => W5_HOME,
+    "SOFTWARE_URL" => W5_SOFTWARE_URL
+                        ));
+
+?>
